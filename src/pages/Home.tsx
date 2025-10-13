faff7a6875215b60dea7d4ec402721ef70bbf2f2
Abhijeet kumar <abhijeetkv2@gmail.com>
2025-10-14 00:08:48 +0530

    feat: add Navbar component and integrate it into Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import ThemeToggle from "../components/ThemeToggle"
 import StarBackground from "../components/StarBackground"
+import Navbar from "../components/Navbar.tsx"
 
 const Home = () => {
   return (
@@ -10,6 +11,8 @@ const Home = () => {
 
         {/* Background Effects */}
         <StarBackground />
+        {/* Navbar */}
+        <Navbar />
     </div>
     </>
   )
